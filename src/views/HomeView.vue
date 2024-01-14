99d2896fde728817aede07b9718dec213d6ace08
Stanislas Ormières <stan@stormier.ninja>
2024-01-14 12:28:48 +0100

    chore: 🎉 create project with vite, vue and sass

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    Test SASS
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  background-color: $bg1;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/variables.sass";\n@import "@/assets/main.sass";'
+      }
+    }
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
